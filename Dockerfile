feea18f5b5d54be0e06f748d464bfbd46291335f
devashish <devashish.86@gmail.com>
2014-10-06 21:46:16 +0530

    finishing up go base image

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,10 @@ RUN apt-get update && apt-get install -y \
 
 RUN curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash;
 
+RUN /bin/bash -c ". /root/.gvm/scripts/gvm && gvm install go1.1";
+RUN /bin/bash -c ". /root/.gvm/scripts/gvm && gvm install go1.2";
+RUN /bin/bash -c ". /root/.gvm/scripts/gvm && gvm install go1.3";
+RUN /bin/bash -c ". /root/.gvm/scripts/gvm && gvm install release";
+RUN /bin/bash -c ". /root/.gvm/scripts/gvm && gvm install tip";
+
 CMD ["/bin/bash"]
--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
-echo "================= Installing gvm ==================="
-apt-get install mercurial make binutils bison gcc build-essential
-sudo su - shippable -c "curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash";
-sudo su - shippable -c "echo '[[ -s \"/home/shippable/.gvm/scripts/gvm\" ]] && source \"/home/shippable/.gvm/scripts/gvm\"' >> /home/shippable/.bashrc";
-
-echo "================= Installing GO 1.1 ==================="
-sudo su - shippable -c "source /home/shippable/.gvm/scripts/gvm && gvm install go1.1";
-
-echo "================= Installing GO 1.2 ==================="
-sudo su - shippable -c "source /home/shippable/.gvm/scripts/gvm && gvm install go1.2";
-
-echo "================= Installing GO release ==================="
-sudo su - shippable -c "source /home/shippable/.gvm/scripts/gvm && gvm install release";
-
-echo "================= Installing GO tip ==================="
-sudo su - shippable -c "source /home/shippable/.gvm/scripts/gvm && gvm install tip";
+#!/bin/bash -e
+VERSION=`cat VERSION`
+TAG="shippableimages/ubuntu1204_go:$VERSION"
+echo "Building $TAG"
+docker build --rm -t $TAG .
